9fb73e1d5dc88dea621f3c17895144b31d783f6f
Flocier886 <3057797906@qq.com>
2026-01-12 19:52:57 +0800

    feat: orm 基础层持久化数据

---
+++ big-market-app/src/test/java/com/flocier/test/infrastructure/AwardDaoTest.java
@@ -0,0 +1,28 @@
+package com.flocier.test.infrastructure;
+
+import com.alibaba.fastjson.JSON;
+import com.flocier.infrastructure.persistent.dao.IAwardDao;
+import com.flocier.infrastructure.persistent.po.Award;
+import lombok.extern.slf4j.Slf4j;
+import org.checkerframework.checker.units.qual.A;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@SpringBootTest
+@Slf4j
+@RunWith(SpringRunner.class)
+public class AwardDaoTest {
+    @Resource
+    private IAwardDao awardDao;
+
+    @Test
+    public void test_queryAwardList(){
+        List<Award> awards=awardDao.queryAwardList();
+        log.info("测试结果: {}", JSON.toJSONString(awards));
+    }
+}
